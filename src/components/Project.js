5fa00a041948a1a82e1a01d5603d2fb1eeebe028
habibwebdev <abbasali211@gmail.com>
2020-12-08 23:54:04 +0500

    Portfolio Page completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import sanityClient from '../client.js'
 
 const Project = () => {
-  return <div>Project</div>
+  const [projectData, setProjectData] = useState(null)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"] | order(date desc) {
+      title,
+      date,
+      place,
+      description,
+      projectType,
+      link,
+      tags
+    }`
+      )
+      .then((data) => setProjectData(data))
+      .catch((error) => console.log(error))
+  }, [])
+
+  return (
+    <main className="min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center mb-4">My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Welcome to my projects page!
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rouned-lg shadow-xl bh-white p-16 bg-green-50">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-green-600">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{' '}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{' '}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{' '}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {' '}
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-green-500 font-bold hover:underline hover:text-red-400 text-lg"
+                  >
+                    View the Project{' '}
+                    <span role="img" aria-label="right pointer">
+                      👉
+                    </span>
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  )
 }
 
 export default Project
