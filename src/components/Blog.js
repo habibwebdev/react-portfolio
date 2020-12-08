248f74c88af24443c32f978af57cc836ebe83104
habibwebdev <abbasali211@gmail.com>
2020-12-08 22:36:16 +0500

    Blog Component completed

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client.js'
 
 const Blog = () => {
-  return <div>Blogs</div>
+  const [postData, setPost] = useState(null)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"] | order(publishedAt desc) {
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`
+      )
+      .then((data) => setPost(data))
+      .catch((error) => console.log(error))
+  }, [])
+
+  return (
+    <main className="min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center">Blog Posts Page</h1>
+        <h2 className="text-lg text-gray-600 justify-center mb-12">
+          Welcome to my page of blog posts
+        </h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={'/post/' + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rouned-r object-cover absolute"
+                    />
+                    <span className="relative h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-gray-100 text-lg font-blog px-3 py-4 bg-green-700 hover:text-green-200 bg-opacity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default Blog
